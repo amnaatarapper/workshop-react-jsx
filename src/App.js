92568d674fb631c0b181fb74f660492e36fd63f2
AIT BAZIZ Oussama <oussama.aitbaziz@imadrassa.com>
2021-01-08 10:44:05 +0100

    map through images

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import images from "./images";
+
 function App() {
   return (
     <div id="wrapper">
@@ -19,126 +21,18 @@ function App() {
       </header>
 
       <div id="main">
-        <article className="thumb">
-          <a href="images/fulls/01.jpg" className="image">
-            <img src="images/thumbs/01.jpg" alt="" />
-          </a>
-          <h2>Magna feugiat lorem</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
-        <article className="thumb">
-          <a href="images/fulls/02.jpg" className="image">
-            <img src="images/thumbs/02.jpg" alt="" />
-          </a>
-          <h2>Nisl adipiscing</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
-        <article className="thumb">
-          <a href="images/fulls/03.jpg" className="image">
-            <img src="images/thumbs/03.jpg" alt="" />
-          </a>
-          <h2>Tempus aliquam veroeros</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
-        <article className="thumb">
-          <a href="images/fulls/04.jpg" className="image">
-            <img src="images/thumbs/04.jpg" alt="" />
-          </a>
-          <h2>Aliquam ipsum sed dolore</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
-        <article className="thumb">
-          <a href="images/fulls/05.jpg" className="image">
-            <img src="images/thumbs/05.jpg" alt="" />
-          </a>
-          <h2>Cursis aliquam nisl</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
-        <article className="thumb">
-          <a href="images/fulls/06.jpg" className="image">
-            <img src="images/thumbs/06.jpg" alt="" />
-          </a>
-          <h2>Sed consequat phasellus</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
-        <article className="thumb">
-          <a href="images/fulls/07.jpg" className="image">
-            <img src="images/thumbs/07.jpg" alt="" />
-          </a>
-          <h2>Mauris id tellus arcu</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
-        <article className="thumb">
-          <a href="images/fulls/08.jpg" className="image">
-            <img src="images/thumbs/08.jpg" alt="" />
-          </a>
-          <h2>Nunc vehicula id nulla</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
-        <article className="thumb">
-          <a href="images/fulls/09.jpg" className="image">
-            <img src="images/thumbs/09.jpg" alt="" />
-          </a>
-          <h2>Neque et faucibus viverra</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
-        <article className="thumb">
-          <a href="images/fulls/10.jpg" className="image">
-            <img src="images/thumbs/10.jpg" alt="" />
-          </a>
-          <h2>Mattis ante fermentum</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
-        <article className="thumb">
-          <a href="images/fulls/11.jpg" className="image">
-            <img src="images/thumbs/11.jpg" alt="" />
-          </a>
-          <h2>Sed ac elementum arcu</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
-        <article className="thumb">
-          <a href="images/fulls/12.jpg" className="image">
-            <img src="images/thumbs/12.jpg" alt="" />
-          </a>
-          <h2>Vehicula id nulla dignissim</h2>
-          <p>
-            Nunc blandit nisi ligula magna sodales lectus elementum non. Integer
-            id venenatis velit.
-          </p>
-        </article>
+        {images.map((img, index) => {
+          const { fullSize, path, title, article } = img;
+          return (
+            <article className="thumb" key={index + 1}>
+              <a href={fullSize} className="image">
+                <img src={path} alt="" />
+              </a>
+              <h2>{title}</h2>
+              <p>{article}</p>
+            </article>
+          );
+        })}
       </div>
 
       <footer id="footer" className="panel">
@@ -159,32 +53,32 @@ function App() {
               <h2>Follow me on ...</h2>
               <ul className="icons">
                 <li>
-                  <a href="#" className="icon brands fa-twitter">
+                  <a href="/" className="icon brands fa-twitter">
                     <span className="label">Twitter</span>
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="icon brands fa-facebook-f">
+                  <a href="/" className="icon brands fa-facebook-f">
                     <span className="label">Facebook</span>
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="icon brands fa-instagram">
+                  <a href="/" className="icon brands fa-instagram">
                     <span className="label">Instagram</span>
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="icon brands fa-github">
+                  <a href="/" className="icon brands fa-github">
                     <span className="label">GitHub</span>
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="icon brands fa-dribbble">
+                  <a href="/" className="icon brands fa-dribbble">
                     <span className="label">Dribbble</span>
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="icon brands fa-linkedin-in">
+                  <a href="/" className="icon brands fa-linkedin-in">
                     <span className="label">LinkedIn</span>
                   </a>
                 </li>
---
+++ src/images.js
@@ -0,0 +1,74 @@
+const images = [
+  {
+    fullSize: "images/fulls/01.jpg",
+    path: "images/thumbs/01.jpg",
+    title: "Magna feugiat lorem",
+    article:
+      "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.",
+  },
+  {
+    fullSize: "images/fulls/02.jpg",
+    path: "images/thumbs/02.jpg",
+    title: "Nisl adipiscing",
+    article:
+      "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.",
+  },
+  {
+    fullSize: "images/fulls/03.jpg",
+    path: "images/thumbs/03.jpg",
+    title: "Tempus aliquam veroeros",
+    article:
+      "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.",
+  },
+  {
+    fullSize: "images/fulls/04.jpg",
+    path: "images/thumbs/04.jpg",
+    title: "Aliquam ipsum sed dolore",
+    article:
+      "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.",
+  },
+  {
+    fullSize: "images/fulls/05.jpg",
+    path: "images/thumbs/05.jpg",
+    title: "Cursis aliquam nisl",
+    article:
+      "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.",
+  },
+  {
+    fullSize: "images/fulls/06.jpg",
+    path: "images/thumbs/06.jpg",
+    title: "Sed consequat phasellus",
+    article:
+      "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.",
+  },
+  {
+    fullSize: "images/fulls/07.jpg",
+    path: "images/thumbs/07.jpg",
+    title: "Mauris id tellus arcu",
+    article:
+      "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.",
+  },
+  {
+    fullSize: "images/fulls/08.jpg",
+    path: "images/thumbs/08.jpg",
+    title: "Nunc vehicula id nulla",
+    article:
+      "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.",
+  },
+  {
+    fullSize: "images/fulls/09.jpg",
+    path: "images/thumbs/09.jpg",
+    title: "Neque et faucibus viverra",
+    article:
+      "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.",
+  },
+  {
+    fullSize: "images/fulls/10.jpg",
+    path: "images/thumbs/10.jpg",
+    title: "Mattis ante fermentum",
+    article:
+      "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.",
+  },
+];
+
+export default images;
